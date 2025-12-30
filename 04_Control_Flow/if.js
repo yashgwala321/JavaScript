15ac1932923f2f343446e3d955465f67ef528503
yashgwala321 <yashgwala4@gmail.com>
2025-12-30 13:41:34 +0530

    Learned JavaScript if-else conditions and logical operators

---
+++ 04_Control_Flow/if.js
@@ -0,0 +1,57 @@
+// if 
+
+const isUserloggedIn = true
+const temperature = 41
+ 
+/*
+if (temperature === 40 ){
+    console.log("less than 50");
+}
+else{
+   console.log("temperature is greater than 50");
+}
+
+
+console.log("Execute");
+*/
+
+// <, >, <=, >=, ==, !=, ===, !==
+
+/*
+const score = 200
+
+if (score > 100) {
+    let power = "fly"
+    console.log(`user power ${power}`);
+}
+    console.log(`user power ${power}`); */
+
+const balance = 1000 
+
+//if (balance > 500) console.log("test"); 
+
+/*
+if (balance < 500) {
+    console.log("less than 500");
+}
+else if (balance < 750) {
+     console.log("less than 500");
+} 
+else if (balance < 900) {
+     console.log("less than 900");
+} 
+else {
+    console.log("less than 1200");
+}*/
+
+const UserLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (UserLoggedIn && debitCard && 2==3) {
+    console.log("Allow to but course");
+}
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("user logged in");
+}
