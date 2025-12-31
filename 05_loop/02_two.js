c1ec5d0def18ce22f920fe100a0b631357ee8e48
yashgwala321 <yashgwala4@gmail.com>
2025-12-31 11:42:43 +0530

    Learned : JavaScript while loop, do-while loop, and array iteration

---
+++ 05_loop/02_two.js
@@ -0,0 +1,28 @@
+// While loop
+let index=0
+while (index <=10 ) {
+    console.log(`value of index is ${index}`);
+    index = index + 2
+} 
+
+//---------------------------------------------------------------------------------------------------------
+
+// Array While Loop
+
+let myArray = ["flash", "btaman", "superman"]
+let arr = 0
+while (arr < myArray.length) {
+    console.log(`value is ${myArray[arr]}`);
+    arr = arr + 1 
+}
+//----------------------------------------------------------------------------------------------------------
+
+//do-while Loop 
+let score = 1
+
+do {
+    console.log(`score is ${score} `);
+    score++
+} while (score <= 10);
+
+//-----------------------------------------------------------------------------------------------------------
